07f026afbfa71f1457dc15b50c5aa1f5af6b3fda
kazuhitokimura <kimura.k@partnersuccess.co.jp>
2023-07-29 03:50:33 +0900

    調整

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ["~/assets/css/destyle.css"],
   vite: {
     css: {
       preprocessorOptions: {
@@ -10,5 +11,4 @@ export default defineNuxtConfig({
       }
     }
   },
-  css: ["~/assets/css/destyle.css"]
 })

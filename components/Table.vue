dbff41612a3f6f5adab5fb8316461ebd963d90e7
kazuhitokimura <kimura.k@partnersuccess.co.jp>
2023-07-31 22:40:59 +0900

    調整

--- components/Table.vue
+++ components/Table.vue
@@ -12,13 +12,13 @@
         <tr>
             <TdText text="テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト" :isNuxtLink="true" nuxtLink="/detail" minWidth="15rem" caption="説明が入りますこれはtruncate不可です"/>
             <TdText text="テキストテキストテキストテキスト"/>
-            <TdBtn :secondBtn="true"/>
+            <TdBtn/>
             <TdBtn/>
         </tr>
         <tr>
             <TdText text="テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト" :isNuxtLink="true" nuxtLink="/detail" minWidth="15rem" :isTruncate="false" caption="説明が入りますこれはtruncate不可です"/>
             <TdText text="テキスト"/>
-            <TdBtn :secondBtn="true"/>
+            <TdBtn/>
             <TdBtn/>
         </tr>
         </tbody>
@@ -33,29 +33,6 @@
         width: 100%;
         border-collapse: collapse;
         border-spacing: 0;
-    }
-
-    .th{
-        padding: 1rem 0.75rem;
-        text-align: left;
-        border-bottom: 1px solid $border-main;
-        color: $text-sub;
-        @include text-h4();
-    }
-
-    .nuxtLink{
-        @include link-base();
-    }
-
-    .captionWrapper{
-        display: flex;
-        flex-direction: column;
-        gap: 4px;
-        width: 100%;
-    }
-    .caption{
-        color: $text-sub;
-        @include text-caption();
     }
 
   </style>

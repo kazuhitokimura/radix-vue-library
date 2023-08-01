e66be012d05add69c0c2d9d4fd3a3130a559c5f1
kazuhitokimura <kimura.k@partnersuccess.co.jp>
2023-08-01 20:18:16 +0900

    textarea

---
+++ components/Textarea.vue
@@ -0,0 +1,55 @@
+<template>
+  <textarea
+    :id="id"
+    :value="value"
+    :placeholder="placeholder"
+    :rows="rows"
+    :class="[$style.textarea, { [$style.error]: isError }]"
+    :disabled="isDisabled"
+  />
+</template>
+
+<script setup>
+const props = defineProps({
+  id: {
+    type: String,
+    default: "",
+  },
+  value: {
+    type: String,
+    default: "",
+  },
+  rows: {
+    type: Number,
+    default: 5,
+  },
+  placeholder: {
+    type: String,
+    default: "プレースホルダー",
+  },
+  isDisabled: {
+    type: Boolean,
+    default: false,
+  },
+  isError: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<style lang="scss" module>
+.textarea {
+  @include input-base();
+
+  &:disabled {
+    opacity: $opacity-disabled;
+    cursor: not-allowed;
+  }
+}
+
+.error {
+  border-color: $error-main;
+  outline: 2px solid $red-100;
+}
+</style>

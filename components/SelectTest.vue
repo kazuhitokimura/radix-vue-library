ae966a285e7a99a192112dd8f568547b5502a1db
kazuhitokimura <kimura.k@partnersuccess.co.jp>
2023-08-19 21:54:42 +0900

    radixのアップデート

---
+++ components/SelectTest.vue
@@ -0,0 +1,68 @@
+<!-- Radix Vueを使用しoptionをfor文にするテスト -->
+
+<template>
+  <SelectRoot>
+    <SelectTrigger :class="$style.selectTrigger" :style="{ width: triggerWidth }">
+      <span :class="$style.selectTriggerText">{{ triggerText }}</span>
+      <Icon icon="radix-icons:chevron-down" :class="$style.selectIcon" />
+    </SelectTrigger>
+    <SelectContent :class="$style.selectContent" :style="{ width: contentWidth }">
+      <slot />
+    </SelectContent>
+  </SelectRoot>
+</template>
+
+<script setup lang="ts">
+import { Icon } from "@iconify/vue";
+
+/* 一応すべてインポートしておく */
+import {
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectItemIndicator,
+  SelectLabel,
+  SelectPortal,
+  SelectRoot,
+  SelectSeparator,
+  SelectTrigger,
+} from "radix-vue";
+
+const props = defineProps({
+  triggerText: {
+    type: String,
+    default: "選択してください",
+  },
+  triggerWidth: {
+    type: String,
+    default: "100%",
+  },
+  contentWidth: {
+    type: String,
+    default: "100%",
+  },
+});
+</script>
+
+<style lang="scss" module>
+.selectTrigger {
+  @include input-base();
+}
+.selectTriggerText {
+  flex: 1;
+  @include text-body1();
+  color: $text-main;
+}
+.selectIcon {
+  width: 1rem;
+  height: 1rem;
+  fill: $text-main;
+}
+.selectContent {
+  margin-top: 0.25rem;
+  padding: 0.25rem 0;
+  background-color: $bg-main;
+  border-radius: $radius-S;
+  box-shadow: $shadow-10;
+}
+</style>
